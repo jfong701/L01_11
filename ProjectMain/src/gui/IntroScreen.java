fa9c4eebaada98c6829595d78d71a239555dfaa3
Mathu-M <mathu.manogaran@gmail.com>
2017-10-22 11:34:22 -0700

    Created a simple GUI screen with temp simple vaildation that moves to new scene

---
+++ ProjectMain/src/gui/IntroScreen.java
@@ -0,0 +1,63 @@
+package gui;
+
+import gui.Professor;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+
+public class IntroScreen extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Intro Screen");
+		
+		// Create and setup grid layout
+		GridPane loginLayout = new GridPane();
+		loginLayout.setPadding(new Insets(15, 15, 15, 15));
+		loginLayout.setVgap(5);
+		loginLayout.setHgap(5);
+		
+		// Username and password labels and inputs
+		Label userLabel = new Label("Username");
+		GridPane.setConstraints(userLabel, 5, 1);
+		
+		TextField userInput = new TextField("Professor");
+		GridPane.setConstraints(userInput, 5, 2);
+		
+		Label passLabel = new Label("Password");
+		GridPane.setConstraints(passLabel, 5, 4);
+		
+		PasswordField passInput = new PasswordField();
+		GridPane.setConstraints(passInput, 5, 5);
+		
+		// Log in button
+		Button loginButton = new Button("Login");
+		loginButton.setOnAction(e -> {
+			if (userInput.getText().equals("Professor")) {
+			Professor.login(primaryStage, userInput.getText(), passInput.getText());
+			}
+		});
+		GridPane.setConstraints(loginButton, 5, 6);
+		
+		
+		loginLayout.getChildren().addAll(userLabel, userInput, passLabel, passInput, loginButton);	
+		Scene loginScene = new Scene(loginLayout, 500, 250);
+		primaryStage.setScene(loginScene);
+		primaryStage.show();
+	}
+
+}
---
+++ ProjectMain/src/gui/Professor.java
@@ -0,0 +1,41 @@
+package gui;
+
+import java.util.Calendar;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+
+public class Professor {
+	public static void login(Stage primaryStage, String user, String pass) {
+		
+		Label welcomeLabel = new Label("Welcome " + user);
+		welcomeLabel.setPadding(new Insets(10, 10, 10, 10));
+		welcomeLabel.setFont(Font.font("Verdana", 20));
+		
+		HBox topBorder = new HBox(50);
+		topBorder.setAlignment(Pos.CENTER_LEFT);
+		topBorder.getChildren().add(welcomeLabel);
+
+		Button addStudents = new Button("Add Students");
+		Button addAssignments = new Button("Add Assginments");
+		
+		VBox centerBorder = new VBox(50);
+		centerBorder.setAlignment(Pos.CENTER);
+		centerBorder.getChildren().addAll(addStudents, addAssignments);
+		
+		BorderPane border = new BorderPane();
+		border.setTop(welcomeLabel);
+		border.setCenter(centerBorder);
+
+		primaryStage.setScene(new Scene(border, 500, 250));
+	}
+}
