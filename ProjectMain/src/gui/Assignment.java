73ccc92bddeb65cb6864cf3a0b8e0d5477daccc0
roach137 <gzhang898@hotmail.com>
2017-10-23 14:47:14 -0400

    Added two java files that define the Question and Assignment data types as well as their constructors and functions

---
+++ ProjectMain/src/gui/Assignment.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+import question
+
+public class Assignment {
+	Question question[];
+	int qCount;
+	public Question(int qCount) {
+		question = new Question [qCount];
+		qCount = qCount;
+	}
+	/*
+	 * initialize a question at index
+	*/
+	public void addQuestion(int index, String q, String ans) {
+		question[i] = Question(q, ans);
+	}
+	/*
+	 * FileWrite (FileWriter outFile)
+	 * Take a valid file writer and write the question's data to the
+	 * file refered to by outFile
+	 */
+	public void fileWrite(FileWriter outFile) {
+		for (int i = 0; i < qCount; i++) {
+			try {
+				question[i].fileWrite(outFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ ProjectMain/src/gui/Question.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Question {
+	String question;
+	String answer;
+	public Question(String q, String ans) {
+		question = q;
+		answer = ans;
+	}
+	/*
+	 * boolean isCorrect
+	   Return if the answer to this question is correct
+	*/
+	public boolean isCorrect(String ans) {
+		return (answer.equals(ans));
+	}
+	/*
+	 * FileWrite (FileWriter outFile)
+	 * Take a valid file writer and write the question's data to the
+	 * file refered to by outFile
+	 */
+	public void fileWrite(FileWriter outFile) {
+		try {
+			outFile.write("\"" + question + "\"," + "\"" + answer + "\"");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
