a5ce93ea0fbbd056db109d094571dee2a03dc6b0
Mathu-M <mathu.manogaran@gmail.com>
2017-11-06 14:13:25 -0500

    Moved Alex's additions from master to this branch

---
+++ CSCC01.sql
@@ -0,0 +1,35 @@
+create database if not exists cscc01;
+use cscc01;
+create table if not exists student (sid varchar(10), 
+									fname varchar(20), 
+									lname varchar(20), 
+                                    utroid varchar(8), 
+                                    student_password varchar(12),
+                                    primary key (sid));
+create table if not exists class (cid varchar(9),
+								  class varchar(10),
+                                  primary key (cid));
+create table if not exists assignment (cid varchar(9),
+									   aid varchar(1),
+                                       assignment varchar(20),
+                                       primary key (cid, aid),
+                                       foreign key (cid) 
+                                       references class (cid));
+create table if not exists question (cid varchar(9),
+								     aid varchar(1),
+                                     qid varchar(2),
+                                     question varchar(30),
+                                     primary key (cid, aid, qid),
+                                     foreign key (cid, aid) references assignment (cid, aid));
+create table if not exists student_assignment (sid varchar(10),
+										       cid varchar(9),
+                                               aid varchar(1),
+                                               mark decimal(5,2) default 0.00,
+                                               primary key (sid, cid, aid),
+                                               foreign key (sid) references student (sid),
+                                               foreign key (cid, aid) references assignment (cid, aid));
+create table if not exists professor (pid varchar(10),
+								      fname varchar(20),
+                                      lname varchar(20),
+                                      professor_password varchar(12),
+                                      primary key (pid));
--- ProjectMain/src/student/Student.java
+++ ProjectMain/src/student/Student.java
@@ -1,4 +1,6 @@
 package student;
+
+import java.sql.*;
 /**
  * 
  */
@@ -23,6 +25,13 @@ public class Student {
 		this.studentLastName = studentLastName;
 	}
 
+	
+	public Student(String studentNo) {
+		this.studentNo = studentNo;
+		
+		this.selectSQL();
+	}
+	
 	
 	/**
 	 * @return the studentNo
@@ -70,8 +79,97 @@ public class Student {
 	public void setStudentLastName(String studentLastName) {
 		this.studentLastName = studentLastName;
 	}
+	
+	
+	public void insertSQL() {
+		String studentUTROID = " ";
+		String studentPassword = " ";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			//Connection conn = DriverManager.getConnection("jdbc:mysql://mathlab.utoronto.ca:3306/cscc43f17_manogar7_sakila?useSSL=False", "manogar7", "manogar7");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/CSCC01?useSSL=False", "root", "MySQL");
+			
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate("insert into student (sid, fname, lname, utroid, student_password)"
+										 + "values ('" + studentNo + "','"
+										 			   + studentFirstName + "','" 
+													   + studentLastName + "','" 
+													   + studentUTROID +"','" 
+													   + studentPassword +"')");
+			
+			conn.close();
+			} catch(SQLException ex) {
+				ex.printStackTrace();
+			} catch (ClassNotFoundException ex) {
+				ex.printStackTrace();
+			}
+	}
 
+	
+	public void deleteSQL() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			//Connection conn = DriverManager.getConnection("jdbc:mysql://mathlab.utoronto.ca:3306/cscc43f17_manogar7_sakila?useSSL=False", "manogar7", "manogar7");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/CSCC01?useSSL=False", "root", "MySQL");
 
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate("delete from student where sid =" + studentNo);
+			
+			conn.close();
+			} catch(SQLException ex) {
+				ex.printStackTrace();
+			} catch (ClassNotFoundException ex) {
+				ex.printStackTrace();
+			}
+	}
+
+	
+	public void selectSQL() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/CSCC01?useSSL=False", "root", "MySQL");
+	
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from student where sid = '" + studentNo + "'");
+			
+			if (rs.next() ) {
+				this.studentFirstName = rs.getString(2);
+				this.studentLastName = rs.getString(3);
+			}
+	
+			conn.close();
+		
+		} catch(SQLException ex) {
+		ex.printStackTrace();
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	
+	public void updateSQL() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/CSCC01?useSSL=False", "root", "MySQL");
+	
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate("update student set fname = '" + this.studentFirstName + "',"
+											    + "lname = '" + this.studentLastName + "'"
+											    + "where sid =" + this.studentNo);
+	
+			conn.close();
+		
+		} catch(SQLException ex) {
+		ex.printStackTrace();
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	
 	/**
 	 * @return string format of this object
 	 */
