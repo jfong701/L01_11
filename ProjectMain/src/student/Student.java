4e06bb8ecec89dcdf73dce9ea64645ad6eaf53d4
Mathu-M <mathu.manogaran@gmail.com>
2017-11-06 10:03:56 -0500

    Revert master back to 9cd1ed9. Alex's files are saved locally and will be added to a branch instead.

--- ProjectMain/src/student/Student.java
+++ ProjectMain/src/student/Student.java
@@ -1,6 +1,4 @@
 package student;
-
-import java.sql.*;
 /**
  * 
  */
@@ -25,13 +23,6 @@ public class Student {
 		this.studentLastName = studentLastName;
 	}
 
-	
-	public Student(String studentNo) {
-		this.studentNo = studentNo;
-		
-		this.selectSQL();
-	}
-	
 	
 	/**
 	 * @return the studentNo
@@ -79,97 +70,8 @@ public class Student {
 	public void setStudentLastName(String studentLastName) {
 		this.studentLastName = studentLastName;
 	}
-	
-	
-	public void insertSQL() {
-		String studentUTROID = " ";
-		String studentPassword = " ";
-		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			//Connection conn = DriverManager.getConnection("jdbc:mysql://mathlab.utoronto.ca:3306/cscc43f17_manogar7_sakila?useSSL=False", "manogar7", "manogar7");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/CSCC01?useSSL=False", "root", "MySQL");
-			
-			Statement stmt = conn.createStatement();
-			stmt.executeUpdate("insert into student (sid, fname, lname, utroid, student_password)"
-										 + "values ('" + studentNo + "','"
-										 			   + studentFirstName + "','" 
-													   + studentLastName + "','" 
-													   + studentUTROID +"','" 
-													   + studentPassword +"')");
-			
-			conn.close();
-			} catch(SQLException ex) {
-				ex.printStackTrace();
-			} catch (ClassNotFoundException ex) {
-				ex.printStackTrace();
-			}
-	}
 
-	
-	public void deleteSQL() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			//Connection conn = DriverManager.getConnection("jdbc:mysql://mathlab.utoronto.ca:3306/cscc43f17_manogar7_sakila?useSSL=False", "manogar7", "manogar7");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/CSCC01?useSSL=False", "root", "MySQL");
 
-			Statement stmt = conn.createStatement();
-			stmt.executeUpdate("delete from student where sid =" + studentNo);
-			
-			conn.close();
-			} catch(SQLException ex) {
-				ex.printStackTrace();
-			} catch (ClassNotFoundException ex) {
-				ex.printStackTrace();
-			}
-	}
-
-	
-	public void selectSQL() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/CSCC01?useSSL=False", "root", "MySQL");
-	
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("select * from student where sid = '" + studentNo + "'");
-			
-			if (rs.next() ) {
-				this.studentFirstName = rs.getString(2);
-				this.studentLastName = rs.getString(3);
-			}
-	
-			conn.close();
-		
-		} catch(SQLException ex) {
-		ex.printStackTrace();
-		} catch (ClassNotFoundException ex) {
-			ex.printStackTrace();
-		}
-	}
-	
-	
-	public void updateSQL() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/CSCC01?useSSL=False", "root", "MySQL");
-	
-			Statement stmt = conn.createStatement();
-			stmt.executeUpdate("update student set fname = '" + this.studentFirstName + "',"
-											    + "lname = '" + this.studentLastName + "'"
-											    + "where sid =" + this.studentNo);
-	
-			conn.close();
-		
-		} catch(SQLException ex) {
-		ex.printStackTrace();
-		} catch (ClassNotFoundException ex) {
-			ex.printStackTrace();
-		}
-	}
-
-	
 	/**
 	 * @return string format of this object
 	 */
