4b739fabf07b54a581e8a5153c455d84040f2621
Jason Fong <jasonc.fong@mail.utoronto.ca>
2017-11-09 01:41:20 -0500

    Question layout now loads based on questions stored in database.
    + Load Label and Field elements in loop instead of fixed layout.
    + Extracts question Num, Question-Answer pairs from database.
    + Minor UI Tweak with Back/Submit buttons
    
    NOTE: Is currently using hard-coded fixed values for Course and
    Assignment (until these parts of the UI are completed)

--- ProjectMain/src/gui/StudentAssignmentPage.java
+++ ProjectMain/src/gui/StudentAssignmentPage.java
@@ -6,6 +6,9 @@ import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import student.Student;
+
+import java.util.ArrayList;
+
 import com.sun.org.apache.xpath.internal.SourceTree;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -21,68 +24,106 @@ import jdbc.DOA;
 public class StudentAssignmentPage {
 
     private static int currentRow;
+
     /**
-     * Creates the StudentAssignmentPage scene where a student can see questions and enter answers for an assignment
+     * Creates the StudentAssignmentPage scene where a student can see questions and
+     * enter answers for an assignment
      *
-     * @param primaryStage   : the existing stage of the program
-     * @param user           : username of student
-     * @param pass           : password of student
-     * @param assignmentName : name of the current assignment being filled out
+     * @param primaryStage
+     *            : the existing stage of the program
+     * @param user
+     *            : username of student
+     * @param pass
+     *            : password of student
+     * @param assignmentName
+     *            : name of the current assignment being filled out
      */
     public static void startAssignment(Stage primaryStage, String user, String pass, String assignmentName) {
 
-        // Create layout
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setPadding(new Insets(15, 15, 15, 15));
-        grid.setVgap(5);
-        grid.setHgap(5);
+	// Create layout
+	GridPane grid = new GridPane();
+	grid.setAlignment(Pos.CENTER);
+	grid.setPadding(new Insets(15, 15, 15, 15));
+	grid.setVgap(5);
+	grid.setHgap(5);
 
-        ScrollPane sp = new ScrollPane(grid);
-        sp.setFitToHeight(true);
-        sp.setFitToWidth(true);
+	ScrollPane sp = new ScrollPane(grid);
+	sp.setFitToHeight(true);
+	sp.setFitToWidth(true);
 
-        Scene addStudentsScene = new Scene(sp, 500, 250);
-        primaryStage.setScene(addStudentsScene);
-        currentRow = 0;
+	Scene addStudentsScene = new Scene(sp, 500, 250);
+	primaryStage.setScene(addStudentsScene);
+	currentRow = 0;
 
-        // Title
-        Text sceneTitle = new Text(assignmentName);
-        sceneTitle.setFont(Font.font("Verdana", FontWeight.NORMAL, 20));
-        grid.add(sceneTitle, 0, currentRow, 2, 1);
-        currentRow++;
+	// System.out.println(DOA.getCourseIds());
+	// System.out.println(DOA.getAssignmentIds("CSCC01"));
 
-        // Questions
-        Label questionNumLabel = new Label("1.");
-        grid.add(questionNumLabel, 0, currentRow, 2, 1);
-        currentRow++;
+	// CHECK how many questions are in the assignment
+	int numQuestions = Integer.parseInt(DOA.QuestionCount("CSCC01", "1"));
 
-        Label questionLabel = new Label("What is 3 + 4?");
-        grid.add(questionLabel, 0, currentRow,2, 1);
-        currentRow++;
+	// Extract questions to fill out labels.
+	ArrayList<ArrayList<String>> questionAndAnswerList = DOA.getQuestions("CSCC01", "1");
 
-        TextField answerField = new TextField();
-        grid.add(answerField, 0, currentRow, 2, 1);
-        currentRow++;
+	String questions[] = new String[numQuestions];
+	String answers[] = new String[numQuestions];
 
-        currentRow += 5;
+	// Extract the questions and answers into local arrays for easier access.
+	for (int i = 0; i < questionAndAnswerList.size(); i++) {
+	    ArrayList<String> qaPair = questionAndAnswerList.get(i);
+	    questions[i] = qaPair.get(0);
+	    answers[i] = qaPair.get(1);
+	}
 
-        Button backButton = new Button("Back");
-        backButton.setOnAction(e -> gui.Student.login(primaryStage, user, pass));
-        grid.add(backButton, 0, currentRow);
+	// Title
+	Label sceneTitle = new Label(assignmentName);
+	sceneTitle.setFont(Font.font("Verdana", FontWeight.NORMAL, 20));
+	grid.add(sceneTitle, 0, currentRow, 2, 1);
+	currentRow++;
 
-        Button submitButton = new Button("Submit");
-        submitButton.setOnAction(e -> {
-            System.out.println("submitted");
-        });
-        grid.add(submitButton, 1, currentRow);
+	// Declare arrays for question numbers, labels, input fields.
+	Label questionNums[];
+	Label questionLabels[];
+	TextField answerFields[];
+
+	questionNums = new Label[numQuestions];
+	questionLabels = new Label[numQuestions];
+	answerFields = new TextField[numQuestions];
+
+	// Create question labels, and textFields
+	for (int i = 0; i < numQuestions; i++) {
+	    questionNums[i] = new Label(Integer.toString(i+1) + ".");
+	    grid.add(questionNums[i], 0, currentRow, 3, 1);
+	    currentRow++;
+
+	    questionLabels[i] = new Label(questions[i]);
+	    grid.add(questionLabels[i], 0, currentRow, 3, 1);
+	    currentRow++;
+
+	    answerFields[i] = new TextField();
+	    grid.add(answerFields[i], 0, currentRow, 3, 1);
+	    currentRow += 2;
+	}
+
+	currentRow += 5;
+
+	Button backButton = new Button("Back");
+	backButton.setOnAction(e -> gui.Student.login(primaryStage, user, pass));
+	grid.add(backButton, 0, currentRow);
+
+	Button submitButton = new Button("Submit");
+	submitButton.setOnAction(e -> {
+	    System.out.println("submitted");
+	});
+	grid.add(submitButton, 2, currentRow);
     }
 
     /**
      * A helper function to add questions to the GUI.
      *
-     * @param questionNumber : Used to create Text Label Question#X
-     * @param question       : Text to be displayed as the question (e.g. "What is 3 + 4?"
+     * @param questionNumber
+     *            : Used to create Text Label Question#X
+     * @param question
+     *            : Text to be displayed as the question (e.g. "What is 3 + 4?"
      */
     private static void addQuestions(int questionNumber, String question, String answer) {
 
