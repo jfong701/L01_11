2fea270f24bd07cc1b9ff78db1224819ddf74691
unknown <alexander.hung@mail.utoronto.ca>
2017-11-09 12:03:40 -0500

    Completed SQL for Classes()

---
+++ ProjectMain/src/student/Classes.java
@@ -0,0 +1,142 @@
+package student;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Classes {
+	String classNo;
+	String className;
+	
+	public Classes(String classNo, String className) {
+		super();
+		this.classNo = classNo;
+		this.className = className;
+	}
+	
+	
+	/**
+	 * Metamorphose Professor() into searching for existing student instead of creating
+	 * new object() into searching for existing student instead of creating new object.
+	 * @param classNo
+	 */
+	public Classes(String classNo) {
+		super();
+		this.classNo = classNo;
+	}
+
+
+	public String getClassNo() {
+		return classNo;
+	}
+
+
+	public void setClassNo(String classNo) {
+		this.classNo = classNo;
+	}
+
+
+	public String getClassName() {
+		return className;
+	}
+
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+
+	public void insertSQL() {
+		try {
+			Class.forName(com.mysql.jdbc.Driver.class.getName());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cscc01?useSSL=false", "root", "MySQL");
+			Statement stmt = conn.createStatement();
+			
+			stmt.executeUpdate("insert into class (cid, name)" +
+											"values ('" + this.classNo +"','" + 
+														  this.className + "')");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void deleteSQL() {
+		try {
+			Class.forName(com.mysql.jdbc.Driver.class.getName());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cscc01?useSSL=false", "root", "MySQL");
+			Statement stmt = conn.createStatement();
+			
+			stmt.executeUpdate("delete from class where cid = '" + this.classNo + "'");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void selectSQL() {
+		try {
+			Class.forName(com.mysql.jdbc.Driver.class.getName());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cscc01?useSSL=false", "root", "MySQL");
+			PreparedStatement pstmt = conn.prepareStatement("select * from class where cid = '" + this.classNo + "'");
+			ResultSet rs = pstmt.executeQuery();
+				
+			if(rs.next()) {
+				this.className = rs.getString(2);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
+	public void updateSQL() {
+		try {
+			Class.forName(com.mysql.jdbc.Driver.class.getName());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cscc01?useSSL=false", "root", "MySQL");
+			Statement stmt = conn.createStatement();
+			
+			stmt.executeUpdate("update class set name = '" + this.className + "'" +
+												"where cid = '" + this.classNo + "'");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Class [classNo=" + classNo + ", className=" + className + "]";
+	}
+}
