704d1d66e9373f4c140af17e9e632362bd329d67
David Yau <david.yau@mail.utoronto.ca>
2017-11-20 16:05:42 -0500

    Finished testing the objects and renamed Test folder to UnitTest.

--- ProjectMain/src/assignment/Assignment.java
+++ ProjectMain/src/assignment/Assignment.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class Assignment {
 	Question question[];
 	int qCount;
-	public void Questions(int qCount) {
+	
+	public Assignment(int qCount) {
 		this.question = new Question [qCount];
 		this.qCount = qCount;
 	}
@@ -15,9 +16,13 @@ public class Assignment {
 	public void addQuestion(int index, String q, String ans) {
 		question[index] = new Question(q, ans);
 	}
+	
+	public Question getQuestion(int index) {
+		return question[index];
+	}
 
 	public static int[] questSet(int n, int max) {
-		int[] set = new int[n];
+		int[] set = new int[3];
 		for (int i=0; i<n; i++) {
 			Random rand = new Random();
 			boolean inSet = false;
