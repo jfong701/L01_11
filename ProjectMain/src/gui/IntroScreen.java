b3ba736f12ef51da6792d2afe21b07fb2d3fb522
Jason Fong <jasonc.fong@mail.utoronto.ca>
2017-11-27 11:47:03 -0500

    Added ability to press Enter on the password field to submit username + password. Also changed title of window

--- ProjectMain/src/gui/IntroScreen.java
+++ ProjectMain/src/gui/IntroScreen.java
@@ -16,6 +16,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.geometry.Insets;
 
 import gui.StudentPage;
@@ -26,29 +27,29 @@ public class IntroScreen extends Application {
 		launch(args);
 
 	}
-	
+
 	public static void startProgram(Stage primaryStage) {
-		primaryStage.setTitle("Intro Screen");
-		
+		primaryStage.setTitle("Angry Sloth");
+
 		// Create and setup grid layout
 		GridPane loginLayout = new GridPane();
 		loginLayout.setPadding(new Insets(15, 15, 15, 15));
 		loginLayout.setVgap(5);
 		loginLayout.setHgap(5);
-		
+
 		// Username and password labels and inputs
 		Label userLabel = new Label("Username");
 		GridPane.setConstraints(userLabel, 5, 1);
-		
-		TextField userInput = new TextField("Professor");
+
+		TextField userInput = new TextField();
 		GridPane.setConstraints(userInput, 5, 2);
-		
+
 		Label passLabel = new Label("Password");
 		GridPane.setConstraints(passLabel, 5, 4);
-		
+
 		PasswordField passInput = new PasswordField();
 		GridPane.setConstraints(passInput, 5, 5);
-		
+
 		// Log in button
 		Button loginButton = new Button("Login");
 		loginButton.setOnAction(e -> {
@@ -64,15 +65,21 @@ public class IntroScreen extends Application {
 				exc.printStackTrace();
 			}
 		});
+
+		// event handler for textfield to click the login Button when Enter is pressed
+		passInput.setOnKeyReleased(e -> {
+			if (e.getCode() == KeyCode.ENTER) {
+				loginButton.fire();
+			}
+		});
 		GridPane.setConstraints(loginButton, 5, 6);
-		//System.out.println(DOA.getAvg("CSCC01", 1));		
-		loginLayout.getChildren().addAll(userLabel, userInput, passLabel, passInput, loginButton);	
+		loginLayout.getChildren().addAll(userLabel, userInput, passLabel, passInput, loginButton);
 		Scene loginScene = new Scene(loginLayout, 500, 250);
 		loginScene.getStylesheets().add("gui/style/css/intro-screen.css");
 		primaryStage.setScene(loginScene);
 		primaryStage.show();
 	}
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception, SQLException {
 		startProgram(primaryStage);
